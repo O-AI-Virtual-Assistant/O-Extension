f28054139ed0ba1d134ae3e828334520c69fbee0
Bassem Ismail <bassem8mohamed8@gmail.com>
2023-12-03 01:44:12 +0200

    create sidebar component

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,20 @@
-import * as vscode from 'vscode';
-import { HelloWorldPanel } from './HelloWorld';
+import * as vscode from "vscode";
+import { HelloWorldPanel } from "./HelloWorld";
+import { SidebarProvider } from "./SidebarProvider";
 
 export function activate(context: vscode.ExtensionContext) {
+  console.log('Congratulations, your extension "O" is now active!');
 
-	console.log('Congratulations, your extension "O" is now active!');
+  const sidebarProvider = new SidebarProvider(context.extensionUri);
+  context.subscriptions.push(
+    vscode.window.registerWebviewViewProvider("O-sidebar", sidebarProvider)
+  );
 
-	context.subscriptions.push(
-		vscode.commands.registerCommand('O.helloWorld', () => {
-			HelloWorldPanel.createOrShow(context.extensionUri);
-		})
-	);
-
+  context.subscriptions.push(
+    vscode.commands.registerCommand("O.helloWorld", () => {
+      HelloWorldPanel.createOrShow(context.extensionUri);
+    })
+  );
 }
 
 export function deactivate() {}
---
+++ webViews/components/Sidebar.svelte
@@ -0,0 +1,8 @@
+<script lang='ts'>
+</script>
+
+<style>
+    
+</style>
+
+<h1> ha ha ha ha ha</h1>
