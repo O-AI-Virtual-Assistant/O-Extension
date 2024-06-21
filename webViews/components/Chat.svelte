0e03fbfd99402826a2455c31774c640fb81847f7
Mariam Elmoazen <11410120200528@stud.cu.edu.eg>
2024-06-21 23:43:17 +0300

    m

--- webViews/components/Chat.svelte
+++ webViews/components/Chat.svelte
@@ -1,10 +1,13 @@
 <script lang="ts">
   import { onMount } from 'svelte';
-
+  import { marked, type Token, type Tokens } from 'marked';
+  import hljs from 'highlight.js';
+  import 'highlight.js/styles/github.css';  // You can choose any other style you like
+  
   let message = '';
   let messages = [];
   let chatbox: HTMLDivElement;
-
+  
   function sendMessage() {
     if (message.trim() !== '') {
       const msg = { content: message, sender: 'user', id: Date.now() };
@@ -14,7 +17,7 @@
       scrollChatToBottom();
     }
   }
-
+  
   function scrollChatToBottom() {
     setTimeout(() => {
       if (chatbox) {
@@ -22,7 +25,18 @@
       }
     }, 100);
   }
-
+  
+  function renderMessage(content) {
+    const renderer = new marked.Renderer();
+  
+    renderer.code= (code, language = '') => {
+      const validLanguage = hljs.getLanguage(language) ? language : 'plaintext';
+      return `<pre><code class="hljs ${validLanguage}">${hljs.highlight(validLanguage, code as any).value}</code></pre>`;
+    };
+  
+    return marked(content, { renderer });
+  }
+  
   onMount(() => {
     window.addEventListener('message', event => {
       const { type, message } = event.data;
@@ -31,175 +45,191 @@
         scrollChatToBottom();
       }
     });
-
+  
     tsvscode.postMessage({ type: 'getMessages', value: undefined });
     scrollChatToBottom();
   });
-</script>
-
-<main>
-  <h1>Chat with O</h1>
-  <div class="chatbox" bind:this={chatbox}>
-    {#each messages as msg (msg.id)}
-      <div class={`message ${msg.sender === 'user' ? 'sent' : 'received'}`}>
-        <p>{msg.content}</p>
-      </div>
-    {/each}
-    {#if messages.length === 0}
-      <div class="instructions">
-        <div class="logo-wrapper">
-          <img src="media/Icon.png" alt="Chat with O Logo" class="chat-logo">
+  </script>
+  
+  <main>
+    <h1>Chat with O</h1>
+    <div class="chatbox" bind:this={chatbox}>
+      {#each messages as msg (msg.id)}
+        <div class={`message ${msg.sender === 'user' ? 'sent' : 'received'}`}>
+          <p>{@html renderMessage(msg.content)}</p>
         </div>
-        <p>Welcome to Chat with O!</p>
-        <p>Type your message below and hit Enter to send.</p>
-        <p>Example queries:</p>
-        <ul>
-          <li>Write me a function to perform Binary Search</li>
-          <li>How to code in Python?</li>
-          <li>Help me fix an error.</li>
-        </ul>
-      </div>
-    {/if}
-  </div>
-  <div class="input-group">
-    <input type="text" bind:value={message} on:keyup="{event => event.key === 'Enter' && sendMessage()}" placeholder="Chat with O...">
-    <button on:click="{sendMessage}" aria-label="Send message">
-      <svg viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-        <line x1="22" y1="2" x2="11" y2="13"></line>
-        <polygon points="22 2 15 22 11 13 2 9 22 2"></polygon>
-      </svg>
-    </button>
-  </div>
-</main>
-
-<style>
-  .logo-wrapper {
-    display: flex;
-    justify-content: center;
-    margin-bottom: 20px;
-  }
-
-  .chat-logo {
-    max-width: 100px;
-    opacity: 0.5;
-  }
-
-  .instructions {
-    padding: 20px;
-    margin-top: 60px;
-    text-align: center;
-    color: white;
-    font-size: 20px;
-  }
-
-  .instructions ul {
-    list-style-type: none;
-    padding: 0;
-  }
-
-  .instructions li {
-    font-size: 15px;
-    margin-bottom: 10px;
-  }
-
-  main {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 20px;
-    height: 100vh;
-    box-sizing: border-box;
-    justify-content: space-between;
-  }
-
-  .chatbox {
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    min-height: 200px;
-    margin-bottom: 10px;
-    padding: 10px;
-    overflow-y: auto;
-    flex-grow: 1;
-  }
-
-  .message {
-    max-width: 80%;
-    margin: 10px;
-    padding: 10px;
-    border-radius: 15px;
-    color: white;
-    word-wrap: break-word;
-  }
-
-  .sent {
-    background-color: #007acc;
-    align-self: flex-end;
-    border-bottom-right-radius: 2px;
-  }
-
-  .received {
-    background-color: #555;
-    align-self: flex-start;
-    border-bottom-left-radius: 2px;
-  }
-
-  .input-group {
-    margin-bottom: 20px;
-    display: flex;
-    width: 100%;
-    align-items: center;
-  }
-
-  input[type="text"], button {
-    height: 40px;
-    display: flex;
-    align-items: center;
-  }
-
-  input[type="text"] {
-    flex-grow: 1;
-    padding: 0 10px;
-    border: 2px solid #ccc;
-    border-right: none;
-    border-radius: 4px 0 0 4px;
-    margin: 0;
-  }
-
-  button {
-    width: 50px;
-    border: 2px solid #ccc;
-    border-left: none;
-    background-color: #007acc;
-    color: white;
-    border-radius: 0 4px 4px 0;
-    cursor: pointer;
-    justify-content: center;
-  }
-
-  button:hover {
-    background-color: #005ea6;
-  }
-
-  button svg {
-    fill: white;
-    width: 20px;
-    height: 20px;
-  }
-
-  @media (max-width: 600px) {
-    .input-group {
+      {/each}
+      {#if messages.length === 0}
+        <div class="instructions">
+          <div class="logo-wrapper">
+            <img src="media/Icon.png" alt="Chat with O Logo" class="chat-logo">
+          </div>
+          <p>Welcome to Chat with O!</p>
+          <p>Type your message below and hit Enter to send.</p>
+          <p>Example queries:</p>
+          <ul>
+            <li>Write me a function to perform Binary Search</li>
+            <li>How to code in Python?</li>
+            <li>Help me fix an error.</li>
+          </ul>
+        </div>
+      {/if}
+    </div>
+    <div class="input-group">
+      <input type="text" bind:value={message} on:keyup="{event => event.key === 'Enter' && sendMessage()}" placeholder="Chat with O...">
+      <button on:click="{sendMessage}" aria-label="Send message">
+        <svg viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+          <line x1="22" y1="2" x2="11" y2="13"></line>
+          <polygon points="22 2 15 22 11 13 2 9 22 2"></polygon>
+        </svg>
+      </button>
+    </div>
+  </main>
+  
+  <style>
+    .logo-wrapper {
+      display: flex;
+      justify-content: center;
+      margin-bottom: 20px;
+    }
+  
+    .chat-logo {
+      max-width: 100px;
+      opacity: 0.5;
+    }
+  
+    .instructions {
+      padding: 20px;
+      margin-top: 60px;
+      text-align: center;
+      color: white;
+      font-size: 20px;
+    }
+  
+    .instructions ul {
+      list-style-type: none;
+      padding: 0;
+    }
+  
+    .instructions li {
+      font-size: 15px;
+      margin-bottom: 10px;
+    }
+  
+    main {
+      display: flex;
       flex-direction: column;
+      align-items: center;
+      padding: 20px;
+      height: 100vh;
+      box-sizing: border-box;
+      justify-content: space-between;
     }
-
-    input[type="text"], button {
+  
+    .chatbox {
+      display: flex;
+      flex-direction: column;
       width: 100%;
-      border-radius: 4px;
+      min-height: 200px;
+      margin-bottom: 10px;
+      padding: 10px;
+      overflow-y: auto;
+      flex-grow: 1;
     }
-
-    button {
+  
+    .message {
+      max-width: 80%;
+      margin: 10px;
+      padding: 10px;
+      border-radius: 15px;
+      color: white;
+      word-wrap: break-word;
+    }
+  
+    .sent {
+      background-color: #007acc;
+      align-self: flex-end;
+      border-bottom-right-radius: 2px;
+    }
+  
+    .received {
+      background-color: #555;
+      align-self: flex-start;
+      border-bottom-left-radius: 2px;
+    }
+  
+    .input-group {
+      margin-bottom: 20px;
+      display: flex;
+      width: 100%;
+      align-items: center;
+    }
+  
+    input[type="text"], button {
       height: 40px;
-      border-top: none;
+      display: flex;
+      align-items: center;
     }
-  }
-</style>
+  
+    input[type="text"] {
+      flex-grow: 1;
+      padding: 0 10px;
+      border: 2px solid #ccc;
+      border-right: none;
+      border-radius: 4px 0 0 4px;
+      margin: 0;
+    }
+  
+    button {
+      width: 50px;
+      border: 2px solid #ccc;
+      border-left: none;
+      background-color: #007acc;
+      color: white;
+      border-radius: 0 4px 4px 0;
+      cursor: pointer;
+      justify-content: center;
+    }
+  
+    button:hover {
+      background-color: #005ea6;
+    }
+  
+    button svg {
+      fill: white;
+      width: 20px;
+      height: 20px;
+    }
+  
+    pre {
+      background-color: #2d2d2d;
+      color: #f8f8f2;
+      padding: 10px;
+      border-radius: 5px;
+      overflow-x: auto;
+      white-space: pre-wrap;
+      word-wrap: break-word;
+    }
+  
+    code {
+      font-family: 'Source Code Pro', monospace;
+      font-size: 1em;
+    }
+  
+    @media (max-width: 600px) {
+      .input-group {
+        flex-direction: column;
+      }
+  
+      input[type="text"], button {
+        width: 100%;
+        border-radius: 4px;
+      }
+  
+      button {
+        height: 40px;
+        border-top: none;
+      }
+    }
+  </style>
+  
